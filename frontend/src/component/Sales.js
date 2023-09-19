fbe495a77459defb3191d02079d22dc22624c4a7
Honi2302 <129046156+Honi2302@users.noreply.github.com>
2023-09-19 22:22:23 +0530

    Update Sales.js

--- frontend/src/component/Sales.js
+++ frontend/src/component/Sales.js
@@ -11,7 +11,7 @@ function Sales() {
 
   const showProducts = async () => {
     try {
-      let result = await fetch("http://localhost:5000/sales");
+      let result = await fetch("https://pharmacy-app-backend.vercel.app/sales");
       let response = await result.json();
       
       // Sort the products array by saleDate in descending order
